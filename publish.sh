36482b03d1d358c6d36d80e69d529ec18cf2b92e
kryptonick <nickmellon@gmail.com>
2026-04-21 12:50:51 +0100

    Update content 2026-04-21 12:50

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+echo "Syncing content from Obsidian..."
+rsync -av --delete \
+  ~/Documents/obsidian/the-gloaming/ \
+  ~/gloaming-wiki/content/
+
+echo "Pushing to GitHub..."
+cd ~/gloaming-wiki
+git add -A
+git commit -m "Update content $(date '+%Y-%m-%d %H:%M')"
+git push github v4
+
+echo "Done! Cloudflare will build and deploy automatically."
